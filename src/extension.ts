7300c59a6f10b36162a192edab0e36ef8d896e25
Ya'suf Dany <yasfdany@gmail.com>
2022-06-28 21:44:50 +0700

    Automacally detect folder path using regex

--- src/extension.ts
+++ src/extension.ts
@@ -15,13 +15,13 @@ function generateClass(){
 		vscode.workspace.openTextDocument(Uri.parse(folder.uri.path+"/pubspec.yaml")).then((document) => {
 			let text = document.getText();
 			let texts = text.split("\n");
-			let startPos = texts.indexOf("    # assets-generator-begin");
-			let endPos = texts.indexOf("    # assets-generator-end");
+
+			var matches = text.match(/(- assets\/)(.*)/g);
 
 			let paths = [];
-			for(var i = startPos+1 ; i < endPos ; i++){
-				paths[i] = texts[i].trim().replace("- ","");
-			}
+			matches.forEach(path => {
+				paths.push(path.trim().replace("- ",""));
+			});
 
 			let fileClass = "";
 			
@@ -30,17 +30,18 @@ function generateClass(){
 					let className = splittedPath[splittedPath.length - 2];
 					className = className.replace(/(\w)(\w*)/g,function(_,g1,g2){return g1.toUpperCase() + g2.toLowerCase();});
 
-					fileClass += "class Asset"+className+" {\n";
-
-					fs.readdirSync(folder.uri.path+"/"+path).forEach(file => {
-						let splittedFilePath = file.split("/");
-						let varName = splittedFilePath[splittedFilePath.length -1].split(".")[0];
-						varName = varName.replace(RegExp("_","g")," ");
-						varName = toCamelCase(varName);
-
-						fileClass += "	static const String "+varName+" = '"+path+file+"';\n";							
-					  });
-					fileClass += "}\n\n";
+					if(fs.existsSync(folder.uri.path+"/"+path)){
+						fileClass += "class Asset"+className+" {\n";
+						fs.readdirSync(folder.uri.path+"/"+path).forEach(file => {
+							let splittedFilePath = file.split("/");
+							let varName = splittedFilePath[splittedFilePath.length -1].split(".")[0];
+							varName = varName.replace(RegExp("_","g")," ");
+							varName = toCamelCase(varName);
+	
+							fileClass += "	static const String "+varName+" = '"+path+file+"';\n";							
+						});
+						fileClass += "}\n\n";
+					}
 				 }
 			);
 
